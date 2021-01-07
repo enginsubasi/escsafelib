68eea414ef3e27248137e3679ff834e7c06e95ac
Engin <enginsubasi@gmail.com>
2021-01-07 19:44:07 +0300

    + Initial commit.

---
+++ template/inc/generic.h
@@ -0,0 +1,29 @@
+${filecomment}
+#ifndef ${include_guard_symbol}
+#define ${include_guard_symbol}
+
+#include <stdint.h>
+
+/* FUNCTION DEFINITIONS */
+
+/* DEFINITIONS */
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+/* TYPEDEFS */
+
+/* STRUCTURES */
+
+/* ENUMS */
+
+/* EXTERNS */
+
+/* FUNCTION PROTOTYPES */
+
+#endif /* ${include_guard_symbol} */
