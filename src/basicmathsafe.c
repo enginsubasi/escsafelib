96d35b2df1688af6dd5f7660aceeb047302d1938
Engin <enginsubasi@gmail.com>
2022-01-25 08:35:37 +0300

    + basicmathsafe files are created.

---
+++ src/basicmathsafe.c
@@ -0,0 +1,25 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      basicmathsafe.c
+  * @author:    Engin Subasi
+  * @email:     enginsubasi@gmail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     25/01/2022
+  * @history:   25/01/2022 Created.
+  *
+  * @about:     Basic mathematics function library file.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+#include "basicmathsafe.h"
+
---
+++ src/selfdiag/selfdiagsafe.c
@@ -0,0 +1,25 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      selfdiagsafe.c
+  * @author:    Engin Subasi
+  * @email:     enginsubasi@gmail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     25/01/2022
+  * @history:   25/01/2022 Created.
+  *
+  * @about:     Self diagnostic function library file without hardware dependencies.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+#include "basicmathsafe.h"
+
